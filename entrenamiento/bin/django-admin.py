a565f758a02486bc2f559520cb89c0bd30c4b11e
dansangar2 <dansangar2@alum.us.es>
2021-02-07 12:56:05 +0100

    Add coverage and requeriments.txt

---
+++ entrenamiento/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/dsanjose/Documentos/Practicando/EGC-1819-dansangar2-prexamen/entrenamiento/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
